0e1bfd7264a233cc7c72243dc3a7358367c779cd
Thomas <thomas.rueegg98@bluewin.ch>
2025-07-20 21:48:03 +0200

    update build script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -2,3 +2,10 @@ pyinstaller --onefile --name zit run_zit.py
 pyinstaller --onefile --name zit-git run_zit_git.py
 pyinstaller --onefile --name zit-fm run_zit_fm.py
 pyinstaller --onefile --name zit-sys run_zit_sys.py
+
+chmod +x dist/zit
+chmod +x dist/zit-git
+chmod +x dist/zit-fm
+chmod +x dist/zit-sys
+
+
