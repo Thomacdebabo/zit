82be9631daa2be668321998f7f3527eb2095e872
Thomas <thomas.rueegg98@bluewin.ch>
2025-09-05 00:36:28 +0200

    simplify calculation by moving calcs to data

--- zit/calculate.py
+++ zit/calculate.py
@@ -1,46 +1,33 @@
 from datetime import datetime
-
-def calculate_interval(event1, event2):
+from zit.events import Event, Project, Subtask, ProjectInterval, ProjectIntervalStorage
+def calculate_interval(event1: Event, event2: Event):
     start_time = event1.timestamp
     end_time = event2.timestamp
     return end_time - start_time
 
 
-def calculate_ongoing_interval(event):
+def calculate_ongoing_interval(event: Event):
     now = datetime.now()
     end_of_day = datetime(now.year, now.month, now.day, 23, 59, 59)
     return min((now - event.timestamp).total_seconds(), (end_of_day - event.timestamp).total_seconds())
 
 def add_project_times(project_time1, project_time2):
-    sum = {}
+    time_sum = {}
     all_keys = set(project_time1.keys()) | set(project_time2.keys())
     for key in all_keys:
-        sum[key] = project_time1.get(key, 0) + project_time2.get(key, 0)
-    return sum
+        time_sum[key] = project_time1.get(key, 0) + project_time2.get(key, 0)
+    return time_sum
 
 def calculate_project_times(events, exclude_projects=[], add_ongoing=True):
     if len(events) == 0:
         return {}, 0, 0
-    sum = 0
-    excluded = 0
-    project_times = {}
-
-    for i in range(1, len(events)):
-        start_event = events[i-1]
-        end_event = events[i]
-        interval = calculate_interval(start_event, end_event)
-
-        project = start_event.name
-        project_times[project] = project_times.get(project, 0) + interval.total_seconds()
-        if project not in exclude_projects:
-            sum += interval.total_seconds()
-        else:
-            excluded += interval.total_seconds()
+    
+    project_interval_storage = ProjectIntervalStorage.from_events(events)
+    project_times = project_interval_storage.calculate_project_times()
 
     if add_ongoing and events[-1].name != "STOP":
         ongoing_interval = calculate_ongoing_interval(events[-1])
         project = events[-1].name
-        project_times[project] = project_times.get(project, 0) + ongoing_interval
-        if project not in exclude_projects:
-            sum += ongoing_interval
-    return project_times, sum, excluded
+        project_times.add_time(project, ongoing_interval)
+    time_sum, excluded = project_times.total_time(exclude_projects=exclude_projects)
+    return project_times.project_times, time_sum, excluded
--- zit/events.py
+++ zit/events.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from tkinter import E
 from pydantic import BaseModel
-from .calculate import *
 import csv
 from pathlib import Path
 
@@ -23,6 +22,10 @@ class Event(BaseModel, ABC):
     @abstractmethod
     def to_row(self):
         pass
+
+class Interval(BaseModel, ABC):
+    start: datetime
+    end: datetime
 
 
 class DataStorage(BaseModel):
@@ -92,6 +95,75 @@ class Project(Event):
 
     def to_row(self):
         return [self.timestamp, self.name]
+    
+class ProjectInterval(Interval):
+    name: str
+    
+    @staticmethod
+    def from_events(start_event: Project, end_event: Project):
+        return ProjectInterval(start=start_event.timestamp, end=end_event.timestamp, name=start_event.name)
+    @property
+    def duration(self):
+        return (self.end - self.start).total_seconds()
+
+
+class ProjectIntervalStorage(BaseModel):
+    intervals: dict[str, list[ProjectInterval]]
+
+    def __init__(self, intervals: list[ProjectInterval]=[]):
+        interval_dict = {}
+        for interval in intervals:
+            if interval.name not in interval_dict:
+                interval_dict[interval.name] = []
+            interval_dict[interval.name].append(interval)
+        super().__init__(intervals=interval_dict)
+
+    @staticmethod
+    def from_events(events: list[Project]):
+        intervals = ProjectIntervalStorage()
+        for i in range(1, len(events)):
+            interval = ProjectInterval.from_events(events[i-1], events[i])
+            intervals.add_interval(interval)
+        return intervals
+
+    def add_interval(self, interval: ProjectInterval):
+        if interval.name not in self.intervals:
+            self.intervals[interval.name] = []
+        self.intervals[interval.name].append(interval)
+    
+    def calculate_project_times(self):
+        return ProjectTimes.from_intervals(self)
+
+class ProjectTimes(BaseModel):
+    project_times: dict[str, float]
+    @staticmethod
+    def from_intervals(intervals: ProjectIntervalStorage):
+        project_times = {}
+        for project, interval_list in intervals.intervals.items():
+            project_times[project] = sum(interval.duration for interval in interval_list)
+        return ProjectTimes(project_times=project_times)
+    
+    def add(self, other):
+        combined_times = {}
+        all_keys = set(self.project_times.keys()) | set(other.project_times.keys())
+        for key in all_keys:
+            combined_times[key] = self.project_times.get(key, 0) + other.project_times.get(key, 0)
+        return ProjectTimes(project_times=combined_times)
+    
+    def add_time(self, project: str, time: float):
+        if project in self.project_times:
+            self.project_times[project] += time
+        else:
+            self.project_times[project] = time
+    def total_time(self, exclude_projects=[]):
+        total_time = 0
+        excluded = 0
+        for project, time in self.project_times.items():
+            if project not in exclude_projects:
+                total_time += time
+            else:
+                excluded += time
+        return total_time, excluded
 
 
 class Subtask(Event):
