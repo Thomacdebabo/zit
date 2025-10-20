74189021c2b000485d76eafd5907117a79362503
thomas <git@thomasengineer.com>
2025-10-20 22:38:03 +0200

    update to 0.6.0

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="zit",
-    version="0.5.2",
+    version="0.6.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
+        "Click",
     ],
     entry_points={
-        'console_scripts': [
-            'zit=zit.cli:cli',
-            'zit-fm=zit.fm.filemanager_cli:fm',
-            'zit-git=zit.git.git_cli:git_cli',
-            'zit-sys=zit.sys.sys_cli:sys_cli',
+        "console_scripts": [
+            "zit=zit.cli:cli",
+            "zit-fm=zit.fm.filemanager_cli:fm",
+            "zit-git=zit.git.git_cli:git_cli",
+            "zit-sys=zit.sys.sys_cli:sys_cli",
         ],
     },
-) 
+)
