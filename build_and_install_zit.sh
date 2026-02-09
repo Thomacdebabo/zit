4564846fb92f4bc9ae0bf2e49a5182827f50d9a7
thomas <git@thomasengineer.com>
2026-02-10 00:20:20 +0100

    implement zted fully within bash

--- build_and_install_zit.sh
+++ build_and_install_zit.sh
@@ -6,3 +6,4 @@ chmod +x dist/zit-python
 sudo rm -f $INSTALL_DIR/zit-python
 sudo cp dist/zit-python $INSTALL_DIR/zit-python
 sudo cp zit-bash/zit $INSTALL_DIR/zit
+sudo cp zit-bash/ted-to-zit $INSTALL_DIR/ted-to-zit
