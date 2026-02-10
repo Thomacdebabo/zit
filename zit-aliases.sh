a882cff549fa3192cbe766c0e14f4b848ff591e7
Thomas <thomas.rueegg@scs.ch>
2026-02-10 10:35:51 +0100

    ad zit aliases

--- zit-aliases.sh
+++ zit-aliases.sh
@@ -1,6 +1,16 @@
-zitls () {
+fzitls () {
 find $HOME/.zit/ -type f -name "*.csv" ! -name "*subtask*" | xargs -I {} basename -s .csv {} | sort -r | fzf --preview="zit list -d {}" --preview-window=right:80% --border
 }
-zitst () {
+fzitst () {
 find $HOME/.zit/ -type f -name "*.csv" ! -name "*subtask*" | xargs -I {} basename -s .csv {} | sort -r | fzf --preview="zit status -d {}" --preview-window=right:80% --border
 }
+
+alias zitls="zit list"
+alias zitst="zit status"
+
+alias zlunch="zit lunch"
+alias zstop="zit stop"
+alias zstart="zit start"
+
+alias zrm="zit remove"
+alias zch="zit change"
